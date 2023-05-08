f0df4dcbd55d66d2788c5f86697ad06d25c33d23
iamjayenmthakuri <malla.jayen@gmail.com>
2023-05-08 09:20:30 +0545

    complete

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+/* eslint-disable react/prop-types */
+import { useCallback, useEffect, useMemo, useState } from "react";
+
+function App({ propA, propB }) {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log("useEffect : propA or propB changed");
+  }, [propA, propB]);
+
+  const memoizedValue = useMemo(() => {
+    console.log(" useMemo : expensive calculation");
+    return propA * 2;
+  }, [propA]);
+
+  const handleClick = useCallback(() => {
+    console.log("useCallback: handleClick called");
+    setCount((prevCount) => prevCount + 1);
+  }, []);
+
+  return (
+    <div>
+      <p>count: {count}</p>
+      <button onClick={handleClick}>Increment Count</button>
+      <p>
+        {" "}
+        PropA * 2:{propA * 2}
+        {memoizedValue}
+      </p>
+    </div>
+  );
+}
+
+export default App;
